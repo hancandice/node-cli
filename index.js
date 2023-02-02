14554d40e28c561d9bddf141e32108130cbda24b
hancandice <64334381+hancandice@users.noreply.github.com>
2023-02-02 10:50:45 +0900

    add readline question logic

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
 console.log("You are my sunshine", process.argv)
+
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("Do you think it is fun? (y/n) ", (answer) => {
+    const response = answer.toLocaleLowerCase()
+    if (response === "y") {
+        console.log("Thank you!")
+    } else if (response === "n") {
+        console.log("Sorry.")
+    } else {
+        console.log("Type only 'y' or 'n'")
+    }
+    rl.close()
+})
